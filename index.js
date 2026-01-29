82da8444d9f30275879224c0b1379542e2a51bab
lazarogiovanni75-source <lazarogiovanni75@gmail.com>
2026-01-28 20:29:53 -0800

    Fix video status route syntax

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const app = express();
 app.use(express.json());
 video/start
-video/status/:jobId
+video/status/jobId
 import draftRoutes from "./routes/draft.js";
 import approvalRoutes from "./routes/approval.js";
 
